dc5e14f249efaed03664053cce8248d6c9ac72ff
evan <mm11751@outlook.com>
2021-11-21 16:29:02 +0800

    添加binding支持

--- app/src/main/java/com/adan/bluetoothtest/MainActivity.java
+++ app/src/main/java/com/adan/bluetoothtest/MainActivity.java
@@ -3,12 +3,17 @@ package com.adan.bluetoothtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.adan.bluetoothtest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        activityMainBinding=ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(activityMainBinding.getRoot());
     }
 }
